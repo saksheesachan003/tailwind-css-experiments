ed41e95692b4c5ef9d13538acad5eb2502e06880
saksheesachan003 <sakshee.sachan003@gmail.com>
2026-02-10 15:32:16 +0530

    added header component

--- TAILWIND_TUT/Tailwind-Concepts/src/App.jsx
+++ TAILWIND_TUT/Tailwind-Concepts/src/App.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import { Header } from './Components/Header'
 
 export default function App() {
   return (
-    <div>
-      
-    </div>
+  <>
+  <Header />
+  </>
   )
 }
---
+++ TAILWIND_TUT/Tailwind-Concepts/src/Components/Header.jsx
@@ -0,0 +1,28 @@
+// [&>*]:flex  -> Provides flex in all child container from parent
+
+export const Header = () => {
+  return (
+    <ul className="bg-black text-white m-0 flex [&>*]:flex  ">
+      <li>
+        <a className="p-3 hover:bg-zinc-700" href="#home">
+          Home
+        </a>
+      </li>
+      <li>
+        <a className="p-3 hover:bg-zinc-700" href="#contact">
+          Contact
+        </a>
+      </li>
+      <li>
+        <a className="p-3 hover:bg-zinc-700" href="#news">
+          News
+        </a>
+      </li>
+      <li>
+        <a className="p-3 hover:bg-zinc-700" href="#about">
+          About
+        </a>
+      </li>
+    </ul>
+  );
+};
