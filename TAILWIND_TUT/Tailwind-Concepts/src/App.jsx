82bec86d63248c381725cd20f9b12225fcdfc635
saksheesachan003 <sakshee.sachan003@gmail.com>
2026-02-10 15:52:50 +0530

    added colors component

--- TAILWIND_TUT/Tailwind-Concepts/src/App.jsx
+++ TAILWIND_TUT/Tailwind-Concepts/src/App.jsx
@@ -1,9 +1,11 @@
+import { Colors } from './Components/Colors'
 import { Header } from './Components/Header'
 
 export default function App() {
   return (
   <>
   <Header />
+  <Colors />
   </>
   )
 }
---
+++ TAILWIND_TUT/Tailwind-Concepts/src/Components/Colors.jsx
@@ -0,0 +1,11 @@
+export const Colors = () => {
+    return (
+        <>
+        <p className="text-blue-400">Text Color In Tailwind CSS</p>
+        <p className="bg-red-400">Background Color In Tailwind CSS</p>
+        <p className="border-green-500 border-2 mt-1">Border In Tailwind CSS</p>
+        <p className="outline-amber-300 outline-4 mt-1">Outline In Tailwind CSS</p>
+        <p className="underline decoration-fuchsia-500">Underline Text</p>
+        </>
+    )
+}
