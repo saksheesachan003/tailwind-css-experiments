768f228aa5ca4a5335781351d1a714fb44f97c60
saksheesachan003 <sakshee.sachan003@gmail.com>
2026-02-10 15:35:12 +0530

    qfix

--- TAILWIND_TUT/Tailwind-Concepts/src/Components/Header.jsx
+++ TAILWIND_TUT/Tailwind-Concepts/src/Components/Header.jsx
@@ -1,8 +1,9 @@
 // [&>*]:flex  -> Provides flex in all child container from parent
+// *:flex -> this is new to apply css in child component from parent
 
 export const Header = () => {
   return (
-    <ul className="bg-black text-white m-0 flex [&>*]:flex  ">
+    <ul className="bg-black text-white m-0 flex *:flex  ">
       <li>
         <a className="p-3 hover:bg-zinc-700" href="#home">
           Home
